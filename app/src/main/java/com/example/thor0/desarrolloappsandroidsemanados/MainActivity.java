5f6421f3b1ea34c4e9f06ef13b9358ccf0c0cd7a
VictorMora <victor.mora.benambourg@gmail.com>
2017-05-09 19:09:30 -0600

    Pantalla principal de ingreso de datos completa

--- app/src/main/java/com/example/thor0/desarrolloappsandroidsemanados/MainActivity.java
+++ app/src/main/java/com/example/thor0/desarrolloappsandroidsemanados/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.thor0.desarrolloappsandroidsemanados;
 
+import android.os.Build;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.DatePicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    //create variables to hold the interface elements
+    private DatePicker dpBirthDate;
+    private TextInputEditText tieUserName;
+    private TextInputEditText tieUserEmail;
+    private TextInputEditText tieUserPhone;
+    private TextInputEditText tieUserDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Initialize object references
+        dpBirthDate = (DatePicker)findViewById(R.id.user_birth_picker);
+        DisableCalendarViewWhenPossible();
+
+        tieUserName = (TextInputEditText)findViewById(R.id.user_name);
+        tieUserPhone = (TextInputEditText)findViewById(R.id.user_phone);
+        tieUserEmail =(TextInputEditText)findViewById(R.id.user_email);
+        tieUserDescription = (TextInputEditText)findViewById(R.id.user_description);
+
     }
+
+    private void DisableCalendarViewWhenPossible(){
+        //check wether the sdk is 11 or higher, the method did not exist before and would
+        //crash the app
+        int currentApiVersion = Build.VERSION.SDK_INT;
+        if(currentApiVersion>= 11){
+            dpBirthDate.setCalendarViewShown(false);
+        }
+
+    }
+
 }
